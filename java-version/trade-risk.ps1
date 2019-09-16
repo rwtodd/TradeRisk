4894da4548533a76f715e1003858e4f549fffd9f
Richard Todd <rwtodd@users.noreply.github.com>
2019-09-15 20:13:23 -0500

    added runner script

---
+++ java-version/trade-risk.ps1
@@ -0,0 +1,6 @@
+java -p target\classes -m org.rwtodd.traderisk/org.rwtodd.traderisk.Cmd
+
+# installed, I use...
+# $BASE="C:\Users\richa\jars"
+# javaw "-Dswing.metalTheme=steel" -p "$BASE\trade-risk" -m org.rwtodd.traderisk @ARGS
+
